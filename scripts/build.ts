40e6bb5ad28f376a5c1e5d4253de34a419ab0f35
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-09-06 20:24:46 +0600

    some refactor and ci fix

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,10 @@
 import { Glob, spawn, spawnSync } from "bun";
-import { rename, rmdir } from "node:fs/promises";
+import { rename, rm } from "node:fs/promises";
 
 const glob = new Glob("dist/*.js");
 
 (async () => {
-  await rmdir("dist", { recursive: true, force: true });
+  await rm("dist", { recursive: true, force: true });
   spawnSync(["bun", "build:esm"]);
 
   const renameTasks = [];
